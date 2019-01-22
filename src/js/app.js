0067ade180271886553cade0f2c31c16638a76ba
wanggege919 <1914161290@qq.com>
2019-01-22 18:34:41 +0800

    webpack初识

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+import module1 from './module-1.js'
+import module2 from './module-2.js'
+import '../css/style.scss'
+
+module1()
+console.log('fuck webpack')
+module2()
+
+
+let a = 123
+console.log(a)
+
